67339d46eb5840cf5933bae42442b451b2c5e0c0
Eldinosaur <eldinosaur7u7@outlook.com>
2022-05-28 02:00:49 -0500

    arreglo de errores para funcionamiento del servidor

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ conectarDB()
 app.use(express.json())
 app.use(cors())
 
-/*app.use('/api/consultas', require('./routes/consultasRoute'))
+app.use('/api/consultas', require('./routes/consultasRoute'))
 app.use('/api/optometras', require('./routes/optometrasRoute'))
 app.use('/api/pacientes', require('./routes/pacientesRoute'))
 app.use('/api/productos', require('./routes/productosRoute'))
-app.use('/api/ventas', require('./routes/ventasRoute'))*/
+app.use('/api/ventas', require('./routes/ventasRoute'))
 
 const port = process.env.PORT  || 8000
 
--- routes/consultasRoute.js
+++ routes/consultasRoute.js
@@ -3,10 +3,10 @@ const express = require('express')
 const router = express.Router()
 const consultasController = require('../controllers/consultasController')
 
-//api/agencias
+//api/
 router.post('/', consultasController.addConsulta)
 router.get('/', consultasController.loadConsultas)
-router.get('/:id', consultasController.loadconsulta)
+router.get('/:id', consultasController.loadConsulta)
 router.put('/:id', consultasController.updateConsulta)
 router.delete('/:id', consultasController.deleteConsulta)
 
